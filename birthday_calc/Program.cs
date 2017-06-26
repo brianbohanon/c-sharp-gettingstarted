070989677d8a626bd74512b0cec3229c84349129
brian.bohanon@gmail.com <cbbohano@aarons.com>
2017-06-26 16:21:25 -0400

    Running birthday calc

---
+++ birthday_calc/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace birthday_calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime birthday = new DateTime(2000, 8, 19);
+
+            TimeSpan countdown = birthday - DateTime.Today;
+            
+            Console.WriteLine($"If you were born on {birthday}, you are {countdown.TotalDays} days old");
+        }
+    }
+}
